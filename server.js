2a3d5801347e9ad89f2d049b7ddf8e0dd8baa449
Jagaesh <charles.techjourney@gmail.com>
2025-07-31 21:29:00 +0200

    set Clarifai API call to backend

--- server.js
+++ server.js
@@ -3,6 +3,57 @@ import cors from 'cors';
 import bcrypt from 'bcrypt-nodejs';
 import knex from 'knex';
 
+const router = express.Router();
+
+// Your PAT (Personal Access Token) can be found in the Account's Security section
+const PAT = 'dd8d4919a3c048f68411e5d7d2f0ef09';
+// Specify the correct user_id/app_id pairings
+const USER_ID = 'jagaesh';
+const APP_ID = 'smartbrain-face-detection';
+// Change these to whatever model and image URL you want to use
+const MODEL_ID = 'face-detection';
+const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
+const IMAGE_URL = 'https://samples.clarifai.com/metro-north.jpg';
+const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
+
+const buildClarifaiRequestOptions = (imageUrl) => {
+  const raw = JSON.stringify({
+    "user_app_id": {
+      "user_id": USER_ID,
+      "app_id": APP_ID
+    },
+    "inputs": [
+      {
+        "data": {
+          "image": {
+            "url": imageUrl
+          }
+        }
+      }
+    ]
+  });
+
+  return ({
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Authorization': 'Key ' + PAT
+    },
+    body: raw
+  });
+}
+
+router.post('/clarifai', (req, res) => {
+  const { imageUrl } = req.body;
+  const clarifaiUrl = `https://api.clarifai.com/v2/models/${MODEL_ID}/versions/${MODEL_VERSION_ID}/outputs`;
+  const requestOptions = buildClarifaiRequestOptions(imageUrl);
+
+  fetch(clarifaiUrl, requestOptions)
+    .then(response => response.json())
+    .then(data => res.json(data))
+    .catch(err => res.status(400).json('Unable to work with Clarifai API'));
+});
+
 const db = knex({
   client: 'pg',
   connection: {
@@ -14,11 +65,13 @@ const db = knex({
   }
 });
 
+
 const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use('/', router);
 
 app.get('/', (req, res) => {
   db('users')
